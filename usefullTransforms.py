945f472041acad561e2f093457de17166500182e
echouni <3135732527@qq.com>
2026-05-23 18:08:41 +0800

    dataset and transform

--- usefullTransforms.py
+++ usefullTransforms.py
@@ -19,5 +19,29 @@ img_norm = trans_norm(img_tensor)
 print(img_norm[0][0][0])
 writer.add_image("Norm", img_norm, 0)
 
+# Resize
+print(img.size)
+trans_resize = transforms.Resize((512,512))
+img_resize = trans_resize(img)
+img_resize = trans_totensor(img_resize)
+writer.add_image("Resize", img_resize, 0)
+print(img_resize)
+
+
+# Compose - resize
+trans_resize_2 = transforms.Resize(512)
+
+trans_compose = transforms.Compose([trans_totensor,trans_resize_2])
+img_resize_2 = trans_compose(img)
+writer.add_image("Resize", img_resize_2, 1)
+
+# RandomCrop 随机裁剪
+trans_random = transforms.RandomCrop(256)
+trans_compose_2 = transforms.Compose([trans_random,trans_totensor])
+for i in range(10):
+    img_crop = trans_compose_2(img)
+    writer.add_image("Crop", img_crop, i)
+
+
 
 writer.close()
